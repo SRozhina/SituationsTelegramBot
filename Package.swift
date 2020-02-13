2a8dbf50fa23597e6e5ef94ce07681224e872ad4
Sofia <sofia.rozhina@gmail.com>
2020-02-13 21:00:38 +0700

    Change sources path

--- Package.swift
+++ Package.swift
@@ -9,8 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .executable(
             name: "SituationsTelegramBot",
-            targets: ["SituationsTelegramBot"],
-            path: "Sources"
+            targets: ["SituationsTelegramBot"]
         ),
     ],
     dependencies: [
@@ -23,6 +22,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SituationsTelegramBot",
-            dependencies: ["TelegramBotSDK"]),
+            dependencies: ["TelegramBotSDK"],
+            path: "Sources"
+        ),
     ]
 )
