03beebc2d81f0347a2ea6bcadb1e1abd80c5beff
Sofia <sofia.rozhina@gmail.com>
2020-02-14 15:52:45 +0700

    Fix

--- Package.swift
+++ Package.swift
@@ -23,7 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SituationsTelegramBot",
-            dependencies: ["TelegramBotSDK, Swifter"],
+            dependencies: ["TelegramBotSDK", "Swifter"],
             path: "Sources"
         ),
     ]
