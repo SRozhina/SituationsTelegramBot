f1110d51b20a624b2ed0f3795f4c842dfaaca2a3
Sofia <sofia.rozhina@gmail.com>
2020-02-18 15:47:30 +0300

    Add multiple users support

--- Sources/Controller.swift
+++ Sources/Controller.swift
@@ -3,32 +3,29 @@ import TelegramBotSDK
 
 class Controller {
     private let router: Router
-    private var situations: [Situation] = []
+    private var situations: [String: [Situation]] = [:]
     
     init(router: Router) {
         self.router = router
         
         router["start"] = { [weak self] context in
-            guard let self = self else { return true }
-            self.handle(context: context, for: self.situations[0])
-            return true
-        }
-    }
-    
-    func handle(update: Update) throws {
-        if let message = update.message,
-            let from = message.from,
-            let username = from.username,
-            situations.isEmpty {
-            situations = getSituations(for: username)
-            for situation in situations {
+            guard let self = self,
+                let username = context.message?.from?.username else { return true }
+            let currentSituations = self.getSituations(for: context.args.scanWord() ?? username)
+            self.situations[username] = currentSituations
+            for situation in currentSituations {
                 router[.callback_query(data: situation.id)] = { [weak self] context in
                     guard let self = self else { return true }
                     self.handle(context: context, for: situation)
                     return true
                 }
             }
+            self.handle(context: context, for: currentSituations[0])
+            return true
         }
+    }
+    
+    func handle(update: Update) throws {
         try router.process(update: update)
     }
     
@@ -46,7 +43,7 @@ class Controller {
     }
     
     private func getSituations(for userName: String) -> [Situation] {
-        guard let fileType = Resources().children.first(where: { $0.filename.contains(userName) }),
+        guard let fileType = Resources().children.first(where: { $0.filename.lowercased().contains(userName.lowercased()) }),
             let file = fileType as? File,
             let data = file.contents,
             let situations = try? JSONDecoder().decode([Situation].self, from: data) else {
